c3719a7a91d3bfa1e8d05801190b312027bf9d7d
augustdemi <augustdemi@gmail.com>
2019-10-28 04:10:37 -0400

    add model to label

--- mnist/main.py
+++ mnist/main.py
@@ -2,7 +2,7 @@ from torchvision import datasets, transforms
 import os
 import torch
 
-from model2 import Encoder, Decoder
+from model import EncoderA, EncoderB, DecoderA, DecoderB
 
 import sys
 sys.path.append('../')
@@ -33,13 +33,13 @@ if __name__ == "__main__":
     parser.add_argument('--lr', type=float, default=1e-3, metavar='LR',
                         help='learning rate [default: 1e-3]')
 
-    parser.add_argument('--label_frac', type=float, default=0.002,
+    parser.add_argument('--label_frac', type=float, default=1,
                         help='how many labels to use')
-    parser.add_argument('--sup_frac', type=float, default=0.02,
+    parser.add_argument('--sup_frac', type=float, default=1,
                         help='supervision ratio')
-    parser.add_argument('--lambda_text', type=float, default=10.,
+    parser.add_argument('--lambda_text', type=float, default=100.,
                         help='multipler for text reconstruction [default: 10]')
-    parser.add_argument('--beta', type=float, default=10.,
+    parser.add_argument('--beta', type=float, default=50.,
                         help='multipler for TC [default: 10]')
 
     parser.add_argument('--ckpt_path', type=str, default='../weights',
@@ -67,10 +67,6 @@ BIAS_TRAIN = (60000 - 1) / (args.batch_size - 1)
 BIAS_TEST = (10000 - 1) / (args.batch_size - 1)
 # model parameters
 NUM_PIXELS = 784
-## model 바꿔서 돌려보기 as marginals.
-NUM_HIDDEN = 256
-NUM_HIDDEN1 = 400
-NUM_HIDDEN2 = 200
 
 
 NUM_SAMPLES = 1
@@ -92,71 +88,85 @@ def cuda_tensors(obj):
         if isinstance(value, torch.Tensor):
             setattr(obj, attr, value.cuda())
 
-enc = Encoder(num_pixels=784, num_hidden1=NUM_HIDDEN1, num_hidden2=NUM_HIDDEN2, zPrivate_dim=args.n_private)
-dec = Decoder(num_pixels=784, num_hidden1=NUM_HIDDEN1, num_hidden2=NUM_HIDDEN2, zPrivate_dim=args.n_private)
+encA = EncoderA(zPrivate_dim=args.n_private)
+decA = DecoderA(zPrivate_dim=args.n_private)
+encB = EncoderB()
+decB = DecoderB()
 if CUDA:
-    enc.cuda()
-    dec.cuda()
-    cuda_tensors(enc)
-    cuda_tensors(dec)
+    encA.cuda()
+    decA.cuda()
+    encB.cuda()
+    decB.cuda()
+    cuda_tensors(encA)
+    cuda_tensors(decA)
+    cuda_tensors(encB)
+    cuda_tensors(decB)
 
-optimizer =  torch.optim.Adam(list(enc.parameters())+list(dec.parameters()),
+optimizer =  torch.optim.Adam(list(encA.parameters())+list(decA.parameters())+list(encB.parameters())+list(decB.parameters()),
                               lr=args.lr)
 
 
-def elbo(iter, q, p, lamb=1.0, beta=(1.0, 1.0, 1.0), bias=1.0, prt=False):
+def elbo(iter, qA, pA, qB, pB, lamb=1.0, beta=(1.0, 1.0, 1.0), bias=1.0):
     # from each of modality
-    reconst_loss_A, kl_A = probtorch.objectives.mws_tcvae.elbo(q, p, p['imagesA'], latents=['privateA', 'sharedA'], sample_dim=0, batch_dim=1,
+    reconst_loss_A, kl_A = probtorch.objectives.mws_tcvae.elbo(qA, pA, pA['images_sharedA'], latents=['private', 'sharedA'], sample_dim=0, batch_dim=1,
                                         lamb=1.0, beta=beta, bias=bias)
+    reconst_loss_B, kl_B = probtorch.objectives.mws_tcvae.elbo(qB, pB, pB['labels_sharedB'], latents=['sharedB'],
+                                                                   sample_dim=0, batch_dim=1,
+                                                                   lamb=lamb, beta=beta, bias=bias)
 
-    if q['poe'] is not None:
+    if qA['poe'] is not None:
         # by POE
         # 기대하는바:sharedA가 sharedB를 따르길. 즉 sharedA에만 digit정보가 있으며, 그 permutataion이 GT에서처럼 identity이기를
-        reconst_loss_poeA, kl_poeA = probtorch.objectives.mws_tcvae.elbo(q, p, p['images_poe'], latents=['privateA', 'poe'], sample_dim=0, batch_dim=1,
+        reconst_loss_poeA, kl_poeA = probtorch.objectives.mws_tcvae.elbo(qA, pA, pA['images_poe'], latents=['private', 'poe'], sample_dim=0, batch_dim=1,
                                                     lamb=1.0, beta=beta, bias=bias)
         # 의미 없음. rec 은 항상 0. 인풋이 항상 GT고 poe결과도 GT를 따라갈 확률이 크기 때문(학습 초반엔 A가 unif이라서, 학습 될수록 A가 B label을 잘 따를테니)
         #loss 값 변화 자체로는 의미 없지만, 이 일정한 loss(GT)가 나오도록하는 sharedA의 back pg에는 의미가짐
-        reconst_loss_poeB, kl_poeB = probtorch.objectives.mws_tcvae.elbo(q, p, q['labels_poe'], latents=['poe'], sample_dim=0, batch_dim=1,
+        reconst_loss_poeB, kl_poeB = probtorch.objectives.mws_tcvae.elbo(qB, pB, pB['labels_poe'], latents=['poe'], sample_dim=0, batch_dim=1,
                                                     lamb=lamb, beta=beta, bias=bias)
 
-        # by cross
-        reconst_loss_crA, kl_crA = probtorch.objectives.mws_tcvae.elbo(q, p, p['images_cross'], latents=['privateA', 'sharedB'], sample_dim=0, batch_dim=1,
-                                                    lamb=1.0, beta=beta, bias=bias)
-        reconst_loss_crB, kl_crB = probtorch.objectives.mws_tcvae.elbo(q, p, q['labels_cross'], latents=['sharedA'], sample_dim=0, batch_dim=1,
-                                                    lamb=lamb, beta=beta, bias=bias)
+        # # by cross
+        # reconst_loss_crA, kl_crA = probtorch.objectives.mws_tcvae.elbo(qA, pA, pA['images_sharedB'], latents=['private', 'sharedB'], sample_dim=0, batch_dim=1,
+        #                                             lamb=1.0, beta=beta, bias=bias)
+        # reconst_loss_crB, kl_crB = probtorch.objectives.mws_tcvae.elbo(qB, pB, pB['labels_sharedA'], latents=['sharedA'], sample_dim=0, batch_dim=1,
+        #                                             lamb=lamb, beta=beta, bias=bias)
 
-        loss = (reconst_loss_A - kl_A) + (reconst_loss_poeA - kl_poeA) + (lamb * reconst_loss_poeB - kl_poeB) + (reconst_loss_crA - kl_crA) + (lamb * reconst_loss_crB - kl_crB)
-        loss = (reconst_loss_poeA - kl_poeA) + (lamb * reconst_loss_poeB - kl_poeB)
-        loss = (reconst_loss_poeA - kl_poeA) + (lamb * reconst_loss_poeB - kl_poeB) + (reconst_loss_crA - kl_crA) + (lamb * reconst_loss_crB - kl_crB)
+        loss = (reconst_loss_A - kl_A) + (lamb * reconst_loss_B - kl_B) + (reconst_loss_poeA - kl_poeA) \
+               + (lamb * reconst_loss_poeB - kl_poeB) \
+               #+ (reconst_loss_crA - kl_crA) + (lamb * reconst_loss_crB - kl_crB)
+        #loss = (reconst_loss_poeA - kl_poeA) + (lamb * reconst_loss_poeB - kl_poeB)
+        #loss = (reconst_loss_poeA - kl_poeA) + (lamb * reconst_loss_poeB - kl_poeB) + (reconst_loss_crA - kl_crA) + (lamb * reconst_loss_crB - kl_crB)
         if iter % 100 == 0:
             print('=========================================')
             print(iter)
             print('reconst_loss_A: ', reconst_loss_A)
             print('kl_A: ', kl_A)
             print('-----------------------------------------')
+            print('reconst_loss_B: ', reconst_loss_B)
+            print('kl_B: ', kl_B)
+            print('-----------------------------------------')
             print('reconst_loss_poeA: ', reconst_loss_poeA)
-            print('reconst_loss_poeA: ', kl_poeA)
+            print('kl_poeA: ', kl_poeA)
             print('-----------------------------------------')
             print('reconst_loss_poeB: ', reconst_loss_poeB)
             print('kl_poeB: ', kl_poeB)
-            print('-----------------------------------------')
-            print('reconst_loss_crA: ', reconst_loss_crA)
-            print('kl_crA: ', kl_crA)
-            print('-----------------------------------------')
-            print('reconst_loss_crB: ', reconst_loss_crB)
-            print('kl_crB: ', kl_crB)
+            # print('-----------------------------------------')
+            # print('reconst_loss_crA: ', reconst_loss_crA)
+            # print('kl_crA: ', kl_crA)
+            # print('-----------------------------------------')
+            # print('reconst_loss_crB: ', reconst_loss_crB)
+            # print('kl_crB: ', kl_crB)
             print('=========================================')
     else:
         loss = 3 * (reconst_loss_A - kl_A)
     return loss
 
-LOSS = 0
-
-def train(data, enc, dec, optimizer,
+def train(data, encA, decA, encB, decB, optimizer,
           label_mask={}, fixed_imgs=None, fixed_labels=None):
     epoch_elbo = 0.0
-    enc.train()
-    dec.train()
+    encA.train()
+    encA.train()
+    decA.train()
+    decA.train()
     N = 0
     torch.autograd.set_detect_anomaly(True)
     for b, (images, labels) in enumerate(data):
@@ -174,19 +184,36 @@ def train(data, enc, dec, optimizer,
                 label_mask[b] = (random() < args.label_frac)
             if label_mask[b]:
                 if args.label_frac == args.sup_frac:
-                    q = enc(images, labels_onehot, num_samples=NUM_SAMPLES)
-                    p = dec(images, {'private': 'privateA', 'shared': 'sharedA'}, out_name='imagesA', q=q,
+                    qA = encA(images, num_samples=NUM_SAMPLES)
+                    qB = encB(labels_onehot, num_samples=NUM_SAMPLES)
+
+                    ## poe ##
+                    prior_logit = torch.zeros_like(qA['sharedA'].dist.logits)  # prior is the concrete dist. of uniform dist.
+                    poe_logit = qA['sharedA'].dist.logits + qB['sharedB'].dist.logits + prior_logit
+                    poe_sample = qA.concrete(logits=poe_logit,
+                               temperature=0.66,
+                               name='poe')
+                    qB.concrete(logits=poe_logit,
+                                temperature=0.66,
+                                value = poe_sample,
+                                name='poe')
+
+                    pA = decA(images, {'sharedA': qA['sharedA'], 'sharedB': qB['sharedB'], 'poe':qA['poe']}, q=qA,
                             num_samples=NUM_SAMPLES)
-                    p = dec(images, {'private': 'privateA', 'shared': 'sharedB'}, out_name='images_cross', q=q, p=p,
+                    pB = decB(labels_onehot, {'sharedA': qA['sharedA'], 'sharedB': qB['sharedB'], 'poe':qB['poe']}, q=qB,
                             num_samples=NUM_SAMPLES)
-                    p = dec(images, {'private': 'privateA', 'shared': 'poe'}, out_name='images_poe', q=q, p=p,
-                            num_samples=NUM_SAMPLES)
-                    loss = -elbo(b, q, p, lamb=args.lambda_text, beta=BETA, bias=BIAS_TRAIN)
+
+                    loss = -elbo(b, qA, pA, qB, pB, lamb=args.lambda_text, beta=BETA, bias=BIAS_TRAIN)
             else:
-                q = enc(images, num_samples=NUM_SAMPLES)
-                p = dec(images, {'private': 'privateA', 'shared': 'sharedA'}, out_name='imagesA', q=q,
-                        num_samples=NUM_SAMPLES)
-                loss = -elbo(b, q, p, lamb=args.lambda_text, beta=BETA, bias=BIAS_TRAIN)
+                qA = encA(images, num_samples=NUM_SAMPLES)
+                qB = encB(labels_onehot, num_samples=NUM_SAMPLES)
+
+                pA = decA(images, {'sharedA': qA['sharedA']}, q=qA,
+                          num_samples=NUM_SAMPLES)
+                pB = decB(labels_onehot, {'sharedB': qB['sharedB']}, q=qB,
+                          num_samples=NUM_SAMPLES)
+
+                loss = -elbo(b, qA, pA, qB, pB, lamb=args.lambda_text, beta=BETA, bias=BIAS_TRAIN)
 
             if args.label_frac < args.sup_frac and random() < args.sup_frac:
                 # print(b)
@@ -200,14 +227,26 @@ def train(data, enc, dec, optimizer,
                     images = images.cuda()
                     labels_onehot = labels_onehot.cuda()
 
-                q = enc(images, labels_onehot, num_samples=NUM_SAMPLES)
-                p = dec(images, {'private': 'privateA', 'shared': 'sharedA'}, out_name='imagesA', q=q,
-                        num_samples=NUM_SAMPLES)
-                p = dec(images, {'private': 'privateA', 'shared': 'sharedB'}, out_name='images_cross', q=q, p=p,
-                        num_samples=NUM_SAMPLES)
-                p = dec(images, {'private': 'privateA', 'shared': 'poe'}, out_name='images_poe', q=q, p=p,
-                        num_samples=NUM_SAMPLES)
-                sup_loss = -elbo(b, q, p, lamb=args.lambda_text, beta=BETA, bias=BIAS_TRAIN)
+                qA = encA(images, num_samples=NUM_SAMPLES)
+                qB = encB(labels_onehot, num_samples=NUM_SAMPLES)
+
+                ## poe ##
+                prior_logit = torch.zeros_like(labels)  # prior is the concrete dist. of uniform dist.
+                poe_logit = qA['sharedA'].dist.logits + qB['sharedB'].dist.logits + prior_logit
+                poe_sample = qA.concrete(logits=poe_logit,
+                                         temperature=0.66,
+                                         name='poe')
+                qB.concrete(logits=poe_logit,
+                            temperature=0.66,
+                            value=poe_sample,
+                            name='poe')
+
+                pA = decA(images, {'sharedA': qA['sharedA'], 'sharedB': qB['sharedB'], 'poe': qA['poe']}, q=qA,
+                          num_samples=NUM_SAMPLES)
+                pB = decB(labels_onehot, {'sharedA': qA['sharedA'], 'sharedB': qB['sharedB'], 'poe': qB['poe']}, q=qB,
+                          num_samples=NUM_SAMPLES)
+
+                sup_loss = -elbo(b, qA, pA, qB, pB, lamb=args.lambda_text, beta=BETA, bias=BIAS_TRAIN)
                 sup_loss.backward()
                 optimizer.step()
 
@@ -224,14 +263,15 @@ def train(data, enc, dec, optimizer,
                 optimizer.step()
                 if CUDA:
                     loss = loss.cpu()
-                LOSS = loss
                 epoch_elbo -= loss.item()
 
     return epoch_elbo / N, label_mask
 
-def test(data, enc, dec, infer=True):
-    enc.eval()
-    dec.eval()
+def test(data, encA, decA, encB, decB, infer=True):
+    encA.eval()
+    decA.eval()
+    encB.eval()
+    decB.eval()
     epoch_elbo = 0.0
     epoch_correct = 0
     N = 0
@@ -241,29 +281,34 @@ def test(data, enc, dec, infer=True):
             images = images.view(-1, NUM_PIXELS)
             if CUDA:
                 images = images.cuda()
-            q = enc(images, num_samples=NUM_SAMPLES)
-            p = dec(images, {'private': 'privateA', 'shared': 'sharedA'}, out_name='imagesA', q=q,
-                    num_samples=NUM_SAMPLES)
-            batch_elbo = elbo(b, q, p, lamb=args.lambda_text, beta=BETA, bias=BIAS_TEST)
+            labels_onehot = torch.zeros(args.batch_size, args.n_shared)
+            labels_onehot.scatter_(1, labels.unsqueeze(1), 1)
+            labels_onehot = torch.clamp(labels_onehot, EPS, 1-EPS)
+
+            qA = encA(images, num_samples=NUM_SAMPLES)
+            qB = encB(labels_onehot, num_samples=NUM_SAMPLES)
+
+            ## poe ##
+            prior_logit = torch.zeros_like(labels)  # prior is the concrete dist. of uniform dist.
+            poe_logit = qA['sharedA'].dist.logits + qB['sharedB'].dist.logits + prior_logit
+            poe_sample = qA.concrete(logits=poe_logit,
+                                     temperature=0.66,
+                                     name='poe')
+            qB.concrete(logits=poe_logit,
+                        temperature=0.66,
+                        value=poe_sample,
+                        name='poe')
+
+            pA = decA(images, {'sharedA': qA['sharedA'], 'sharedB': qB['sharedB'], 'poe': qA['poe']}, q=qA,
+                      num_samples=NUM_SAMPLES)
+            pB = decB(labels_onehot, {'sharedA': qA['sharedA'], 'sharedB': qB['sharedB'], 'poe': qB['poe']}, q=qB,
+                      num_samples=NUM_SAMPLES)
+
+            batch_elbo = elbo(b, qA, pA, qB, pB, lamb=args.lambda_text, beta=BETA, bias=BIAS_TRAIN)
             if CUDA:
                 batch_elbo = batch_elbo.cpu()
             epoch_elbo += batch_elbo.item()
-            if infer:
-                log_p = p.log_joint(0, 1)
-                log_q = q.log_joint(0, 1)
-                log_w = log_p - log_q
-                w = torch.nn.functional.softmax(log_w, 0)
-                y_samples = q['sharedA'].value
-                y_expect = (w.unsqueeze(-1) * y_samples).sum(0)
-                _ , y_pred = y_expect.max(-1)
-                if CUDA:
-                    y_pred = y_pred.cpu()
-                epoch_correct += (labels == y_pred).sum().item()
-            else:
-                _, y_pred = q['sharedA'].value.max(-1)
-                if CUDA:
-                    y_pred = y_pred.cpu()
-                epoch_correct += (labels == y_pred).sum().item() / (NUM_SAMPLES or 1.0)
+            epoch_correct += pB['labels_sharedA'].loss.sum().item()
     return epoch_elbo / N, epoch_correct / N
 
 
@@ -301,8 +346,10 @@ import time
 from random import random
 
 if args.ckpt_epochs > 0:
-    enc.load_state_dict(torch.load('%s/%s-enc_epoch%s.rar' % (args.ckpt_path, MODEL_NAME, args.ckpt_epochs)))
-    dec.load_state_dict(torch.load('%s/%s-dec_epoch%s.rar' % (args.ckpt_path, MODEL_NAME, args.ckpt_epochs)))
+    encA.load_state_dict(torch.load('%s/%s-encA_epoch%s.rar' % (args.ckpt_path, MODEL_NAME, args.ckpt_epochs)))
+    decA.load_state_dict(torch.load('%s/%s-decA_epoch%s.rar' % (args.ckpt_path, MODEL_NAME, args.ckpt_epochs)))
+    encB.load_state_dict(torch.load('%s/%s-encB_epoch%s.rar' % (args.ckpt_path, MODEL_NAME, args.ckpt_epochs)))
+    decB.load_state_dict(torch.load('%s/%s-decB_epoch%s.rar' % (args.ckpt_path, MODEL_NAME, args.ckpt_epochs)))
 
 mask = {}
 
@@ -313,14 +360,14 @@ if args.label_frac < args.sup_frac:
 for e in range(args.ckpt_epochs, args.epochs):
     train_start = time.time()
     if args.label_frac < args.sup_frac:
-        train_elbo, mask = train(train_data, enc, dec,
+        train_elbo, mask = train(train_data, encA, decA, encB, decB,
                                  optimizer, mask, fixed_imgs=fixed_imgs, fixed_labels=fixed_labels)
     else:
-        train_elbo, mask = train(train_data, enc, dec,
+        train_elbo, mask = train(train_data, encA, decA, encB, decB,
                                  optimizer, mask)
     train_end = time.time()
     test_start = time.time()
-    test_elbo, test_accuracy = test(test_data, enc, dec)
+    test_elbo, test_accuracy = test(test_data, encA, decA, encB, decB,)
     test_end = time.time()
     print('[Epoch %d] Train: ELBO %.4e (%ds) Test: ELBO %.4e, Accuracy %0.3f (%ds)' % (
             e, train_elbo, train_end - train_start,
@@ -328,10 +375,15 @@ for e in range(args.ckpt_epochs, args.epochs):
 
 if not os.path.isdir(args.ckpt_path):
     os.mkdir(args.ckpt_path)
-torch.save(enc.state_dict(),
-           '%s/%s-enc_epoch%s.rar' % (args.ckpt_path, MODEL_NAME, args.epochs ))
-torch.save(dec.state_dict(),
-           '%s/%s-dec_epoch%s.rar' % (args.ckpt_path, MODEL_NAME, args.epochs))
+torch.save(encA.state_dict(),
+           '%s/%s-encA_epoch%s.rar' % (args.ckpt_path, MODEL_NAME, args.epochs))
+torch.save(decA.state_dict(),
+           '%s/%s-decA_epoch%s.rar' % (args.ckpt_path, MODEL_NAME, args.epochs))
+torch.save(encB.state_dict(),
+           '%s/%s-encB_epoch%s.rar' % (args.ckpt_path, MODEL_NAME, args.epochs))
+torch.save(decB.state_dict(),
+           '%s/%s-decB_epoch%s.rar' % (args.ckpt_path, MODEL_NAME, args.epochs))
+
 
 print('[encoder] ELBO: %e, ACCURACY: %f' % test(test_data, enc, dec, infer=False))
 print('[encoder+inference] ELBO: %e, ACCURACY: %f' % test(test_data, enc, dec, infer=True))
--- mnist/model.py
+++ mnist/model.py
@@ -12,15 +12,15 @@ import probtorch
 from probtorch.util import expand_inputs
 
 EPS = 1e-9
+TEMP = 0.66
 
-
-class Encoder(nn.Module):
+class EncoderA(nn.Module):
     def __init__(self, num_pixels=784,
                        num_hidden=256,
                        zShared_dim=10,
                      zPrivate_dim=50):
         super(self.__class__, self).__init__()
-
+        self.digit_temp = torch.tensor(TEMP)
         self.zPrivate_dim = zPrivate_dim
         self.zShared_dim = zShared_dim
 
@@ -29,11 +29,11 @@ class Encoder(nn.Module):
             nn.ReLU())
 
         self.fc  = nn.Linear(num_hidden, 2*zPrivate_dim + zShared_dim)
-        self.digit_temp = torch.tensor(0.66)
 
     @expand_inputs
-    def forward(self, x, labels=None, num_samples=None):
+    def forward(self, x, num_samples=None):
         q = probtorch.Trace()
+
         hiddens = self.enc_hidden(x)
         stats = self.fc(hiddens)
 
@@ -45,40 +45,20 @@ class Encoder(nn.Module):
 
         q.normal(loc=muPrivate,
                  scale=stdPrivate,
-                 name='privateA')
-
+                 name='private')
         q.concrete(logits=shared_logit,
                     temperature=self.digit_temp,
                     name='sharedA')
-        if labels is not None:
-            alpha_logit = shared_logit
-            beta_logit = torch.log(labels + EPS) # param for concrete dist. should be bigger than 0
-            prior_logit = torch.zeros_like(labels) # prior is the concrete dist. for uniform dist.
-            poe_logit = torch.pow(alpha_logit + beta_logit + prior_logit, 3)
-
-            q.concrete(logits=poe_logit,
-                       temperature=self.digit_temp,
-                       name='poe') # will follow sharedB since sharedB is obtained by identity map
-
-            q.concrete(logits=beta_logit,
-                       temperature=self.digit_temp,
-                       value=labels,
-                       name='sharedB')
-
-            label_loss = lambda y_pred, target: (1 - (target == y_pred).float())
-            q.loss(label_loss, q['sharedA'].value.max(-1)[1], labels.max(-1)[1], name='labels_cross')
-            # print(torch.log( (q['sharedA'].value.max(-1)[1] == labels.max(-1)[1]).float() + 1e-9))
-            q.loss(label_loss, q['poe'].value.max(-1)[1], labels.max(-1)[1], name='labels_poe')
         return q
 
 
-class Decoder(nn.Module):
+class DecoderA(nn.Module):
     def __init__(self, num_pixels=784,
                        num_hidden=256,
                     zShared_dim=10,
                     zPrivate_dim=50):
         super(self.__class__, self).__init__()
-        self.digit_temp = 0.66
+        self.digit_temp = TEMP
 
         self.style_mean = zPrivate_dim
         self.style_std = zPrivate_dim
@@ -91,34 +71,103 @@ class Decoder(nn.Module):
                            nn.Linear(num_hidden, num_pixels),
                            nn.Sigmoid())
 
-    def forward(self, images, latents, out_name, q=None, p=None, num_samples=None):
+    def forward(self, images, shared, q=None, p=None, num_samples=None):
 
+        digit_log_weights = torch.zeros_like(q['sharedA'].dist.logits) # prior is the concrete dist for uniform dist. with all params=1
+        style_mean = torch.zeros_like(q['private'].dist.loc)
+        style_std = torch.ones_like(q['private'].dist.scale)
 
-        private=latents['private']
-        shared=latents['shared']
-        digit_log_weights = torch.zeros_like(q[shared].dist.logits) # prior is the concrete dist for uniform dist. with all params=1
-        style_mean = torch.zeros_like(q[private].dist.loc)
-        style_std = torch.ones_like(q[private].dist.scale)
+        p = probtorch.Trace()
 
-        if p is None:
-            p = probtorch.Trace()
-            p.normal(style_mean,
-                            style_std,
-                            value=q[private],
-                            name=private)
-        zPrivate = p[private].value
-        zShared = p.concrete(logits=digit_log_weights,
-                            temperature=self.digit_temp,
-                            value=q[shared],
-                            name=shared)
-        if shared == 'poe':
-            hiddens = self.dec_hidden(torch.cat([torch.pow(zShared + EPS, 1/3), zPrivate], -1))
-            # hiddens = self.dec_hidden(torch.cat([zShared, zPrivate], -1))
-        else:
-            hiddens = self.dec_hidden(torch.cat([zShared, zPrivate], -1))
-        images_mean = self.dec_image(hiddens)
-        # define reconstruction loss (log prob of bernoulli dist)
-        p.loss(lambda x_hat, x: -(torch.log(x_hat + EPS) * x +
-                                  torch.log(1 - x_hat + EPS) * (1-x)).sum(-1),
-               images_mean, images, name=out_name)
+        # prior for z_private
+        zPrivate = p.normal(style_mean,
+                        style_std,
+                        value=q['private'],
+                        name='private')
+        # private은 sharedA(infA), sharedB(crossA), sharedPOE 모두에게 공통적으로 들어가는 node로 z_private 한 샘플에 의해 모두가 다 생성돼야함
+        for shared_name in shared.keys():
+            # prior for z_shared
+            zShared = p.concrete(logits=digit_log_weights,
+                                temperature=self.digit_temp,
+                                value=shared[shared_name],
+                                name=shared_name)
+
+            if 'poe' in shared_name:
+                hiddens = self.dec_hidden(torch.cat([zPrivate, torch.pow(zShared + EPS, 1/3)], -1))
+            else:
+                hiddens = self.dec_hidden(torch.cat([zPrivate, zShared], -1))
+
+            images_mean = self.dec_image(hiddens)
+
+            # define reconstruction loss (log prob of bernoulli dist)
+            p.loss(lambda x_hat, x: -(torch.log(x_hat + EPS) * x +
+                                      torch.log(1 - x_hat + EPS) * (1-x)).sum(-1),
+                   images_mean, images, name= 'images_' + shared_name)
         return p
+
+class EncoderB(nn.Module):
+    def __init__(self, num_digis=10,
+                       num_hidden=256,
+                       zShared_dim=10):
+        super(self.__class__, self).__init__()
+        self.digit_temp = torch.tensor(TEMP)
+        self.zShared_dim = zShared_dim
+
+        self.enc_hidden = nn.Sequential(
+            nn.Linear(num_digis, num_hidden),
+            nn.ReLU())
+
+        self.fc  = nn.Linear(num_hidden, zShared_dim)
+
+
+    @expand_inputs
+    def forward(self, labels, num_samples=None):
+        q = probtorch.Trace()
+        hiddens = self.enc_hidden(labels)
+        shared_logit = self.fc(hiddens)
+
+        q.concrete(logits=shared_logit,
+                    temperature=self.digit_temp,
+                    name='sharedB')
+        return q
+
+
+
+class DecoderB(nn.Module):
+    def __init__(self, num_digits=10,
+                       num_hidden=256,
+                    zShared_dim=10):
+        super(self.__class__, self).__init__()
+        self.digit_temp = TEMP
+        self.num_digits = zShared_dim
+
+        self.dec_hidden = nn.Sequential(
+                            nn.Linear(zShared_dim, num_hidden),
+                            nn.ReLU())
+        self.dec_label = nn.Sequential(
+                           nn.Linear(num_hidden, num_digits),
+                            nn.Softmax())
+
+    def forward(self, labels, shared, q=None, p=None, num_samples=None):
+        p = probtorch.Trace()
+
+        # private은 sharedA(infA), sharedB(crossA), sharedPOE 모두에게 공통적으로 들어가는 node로 z_private 한 샘플에 의해 모두가 다 생성돼야함
+        for shared_name in shared.keys():
+            # prior for z_shared # prior is the concrete dist for uniform dist. with all params=1
+            zShared = p.concrete(logits=torch.zeros_like(q['sharedB'].dist.logits),
+                                temperature=self.digit_temp,
+                                value=shared[shared_name],
+                                name=shared_name)
+
+            if 'poe' in shared_name:
+                hiddens = self.dec_hidden(torch.pow(zShared + EPS, 1/3))
+            else:
+                hiddens = self.dec_hidden(zShared)
+
+            pred_labels = self.dec_label(hiddens)
+
+            # define reconstruction loss (log prob of bernoulli dist)
+            p.loss(lambda y_pred, target: (1 - (target == y_pred).float()), \
+                   pred_labels.max(-1)[1], labels.max(-1)[1], name='labels_' + shared_name)
+        return p
+
