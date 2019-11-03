0b8bfa79648d64eb567767a37b1a4506cdeb55c3
augustdemi <augustdemi@gmail.com>
2019-11-03 18:56:15 -0500

    bias default none -> 1.0

--- probtorch/objectives/mws_tcvae.py
+++ probtorch/objectives/mws_tcvae.py
@@ -1,19 +1,10 @@
-import torch
-from torch.autograd import Variable
-from torch.distributions.relaxed_categorical import RelaxedOneHotCategorical
-from torch.distributions.normal import Normal
-
-
-def elbo(q, p, rec, latents=None, sample_dim=None, batch_dim=None, lamb=1.0, beta=[1.0,1.0,1.0],
-         size_average=True, bias=None):
+def elbo(q, p, rec, latents=None, sample_dim=None, batch_dim=None, beta=[1.0,1.0,1.0],
+         bias=1.0):
     reconst_loss = rec.loss
 
     log_pz, log_qz, log_prod_qzi, log_q_zCx = _get_probability(q, p, latents, sample_dim, batch_dim, bias)
     kl = beta[0] * (log_q_zCx - log_qz) +  beta[1] * (log_qz - log_prod_qzi) + beta[2] * (log_prod_qzi - log_pz)
 
-    # elbo = lamb * reconst_loss - kl
-    # elbo = elbo.mean(sample_dim) # across sample_dim
-    # elbo = elbo.mean() if size_average else elbo.sum() # avg = across batch_dim
     return (reconst_loss.mean(), kl.mean())
 
 def _get_probability(q, p, latents, sample_dim, batch_dim, bias):
@@ -22,26 +13,3 @@ def _get_probability(q, p, latents, sample_dim, batch_dim, bias):
     log_joint_qz_marginal, _, log_prod_qzi = q.log_batch_marginal(sample_dim, batch_dim, latents, bias=bias)
     return log_pz, log_joint_qz_marginal, log_prod_qzi, log_q_zCx
 
-
-#
-#
-# def _get_prior_params(latent_node):
-#     if isinstance(latent_node.dist, RelaxedOneHotCategorical):
-#         zeros = torch.zeros(latent_node.value.shape[1:])
-#     else:
-#         assert isinstance(latent_node.dist, Normal)
-#         zeros = torch.zeros(latent_node.value.shape[1:] + (2,))
-#     prior_params = Variable(zeros)
-#     return prior_params
-#
-# def _log_importance_weight_matrix(self, batch_size, dataset_size):
-#     N = dataset_size
-#     M = batch_size - 1
-#     strat_weight = (N - M) / (N * M)
-#     W = torch.Tensor(batch_size, batch_size).fill_(1 / M)
-#     W.view(-1)[::M+1] = 1 / N
-#     W.view(-1)[1::M+1] = strat_weight
-#     W[M-1, 0] = strat_weight
-#     return W.log()
-#
-#
