445059d358ef42c834bc3d0a2b8b591b60572bca
augustdemi <augustdemi@gmail.com>
2019-11-06 02:59:53 -0500

    add solver_test

--- util/__init__.py
+++ util/__init__.py
@@ -1,6 +1,7 @@
 from . import evaluation
 from .evaluation import *
+from .solver_test import *
 # from . import dataset
 # from .dataset import *
-__all__ = ["evaluation"]
-# __all__.extend(dataset.__all__)
+__all__ = ["evaluation", "solver_test"]
+# __all__.extend(solver_test.__all__)
