5f57c542d6169a973809c6d7d9c4a58f1c2c2a3c
augustdemi <augustdemi@gmail.com>
2019-11-07 03:11:44 -0500

    revert to previous

--- dsprite/model_conv.py
+++ dsprite/model_conv.py
@@ -191,10 +191,9 @@ class EncoderB(nn.Module):
         logvarShared = stats[:, :, (2 * self.zPrivate_dim + self.zShared_dim):]
         stdShared = torch.sqrt(torch.exp(logvarShared) + EPS)
 
-        if self.zPrivate_dim > 0:
-            q.normal(loc=muPrivate,
-                     scale=stdPrivate,
-                     name='privateB')
+        q.normal(loc=muPrivate,
+                 scale=stdPrivate,
+                 name='privateB')
         q.normal(loc=muShared,
                  scale=stdShared,
                  name='sharedB')
@@ -232,11 +231,10 @@ class DecoderB(nn.Module):
         p = probtorch.Trace()
 
         # prior for z_private
-        if self.zPrivate_dim > 0:
-            zPrivate = p.normal(priv_mean,
-                                priv_std,
-                            value=q['privateB'],
-                            name='privateB')
+        zPrivate = p.normal(priv_mean,
+                            priv_std,
+                        value=q['privateB'],
+                        name='privateB')
         # private은 sharedA(infA), sharedB(crossA), sharedPOE 모두에게 공통적으로 들어가는 node로 z_private 한 샘플에 의해 모두가 다 생성돼야함
         for shared_name in shared.keys():
             # prior for z_shared
