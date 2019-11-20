64762ac2814a8cd8fe5724bb3cbe3696e166684c
augustdemi <augustdemi@gmail.com>
2019-11-20 03:10:01 -0500

    save bug

--- svhn_mnist/main.py
+++ svhn_mnist/main.py
@@ -21,7 +21,7 @@ import util
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('--run_id', type=int, default=0, metavar='N',
+    parser.add_argument('--run_id', type=int, default=1, metavar='N',
                         help='run_id')
     parser.add_argument('--run_desc', type=str, default='',
                         help='run_id desc')
@@ -31,9 +31,9 @@ if __name__ == "__main__":
                         help='size of the latent embedding of private')
     parser.add_argument('--batch_size', type=int, default=100, metavar='N',
                         help='input batch size for training [default: 100]')
-    parser.add_argument('--ckpt_epochs', type=int, default=15, metavar='N',
+    parser.add_argument('--ckpt_epochs', type=int, default=20, metavar='N',
                         help='number of epochs to train [default: 200]')
-    parser.add_argument('--epochs', type=int, default=15, metavar='N',
+    parser.add_argument('--epochs', type=int, default=20, metavar='N',
                         help='number of epochs to train [default: 200]')
     parser.add_argument('--lr', type=float, default=1e-3, metavar='LR',
                         help='learning rate [default: 1e-3]')
@@ -340,8 +340,9 @@ def test(data, encA, decA, encB, decB, epoch):
 
 
     if (epoch+1) % 5 ==  0 or epoch+1 == args.epochs:
-        util.evaluation.save_traverse_both(args.epochs, test_data, encA, decA, encB, decB, CUDA,
-                                           output_dir_trvsl=MODEL_NAME, flatten_pixel=NUM_PIXELS, fixed_idxs=[0,600,10000,12000,16000,18000,19000,21000,23000,25000])
+        util.evaluation.save_traverse_both(epoch, test_data, encA, decA, encB, decB, CUDA,
+                                           output_dir_trvsl=MODEL_NAME, flatten_pixel=NUM_PIXELS,
+                                           fixed_idxs=[0, 600, 10000, 12000, 16001, 18000, 19000, 21000, 23000, 25000])
 
         save_ckpt(e+1)
     return epoch_elbo / N, 1 + epoch_correct / N
@@ -438,7 +439,7 @@ if args.ckpt_epochs == args.epochs:
 
     util.evaluation.save_traverse_both(args.epochs, test_data, encA, decA, encB, decB, CUDA,
                                        output_dir_trvsl=MODEL_NAME, flatten_pixel=NUM_PIXELS,
-                                       fixed_idxs=[0, 6000, 10000, 12000, 16000, 18000, 19000, 21000, 23000, 25000])
+                                       fixed_idxs=[0, 6000, 10000, 12000, 16001, 18000, 19000, 21000, 23000, 25000])
     # util.evaluation.save_reconst(args.epochs, test_data, encA, decA, encB, decB, CUDA, fixed_idxs=[21, 2, 1, 10, 14, 25, 17, 86, 9, 50], output_dir_trvsl=MODEL_NAME)
     # util.evaluation.mutual_info(test_data, encA, CUDA, flatten_pixel=NUM_PIXELS)
 else:
