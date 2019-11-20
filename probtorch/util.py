54464649bc6705da467d2c40cb8ed5796e5de636
augustdemi <augustdemi@gmail.com>
2019-11-20 17:19:17 -0500

    kaiming_init

--- probtorch/util.py
+++ probtorch/util.py
@@ -189,6 +189,17 @@ def normal_init(m):
             m.bias.data.fill_(0)
 
 
+def kaiming_init(m):
+    if isinstance(m, (nn.Linear, nn.Conv2d)):
+        init.kaiming_normal_(m.weight)
+        if m.bias is not None:
+            m.bias.data.fill_(0)
+    elif isinstance(m, (nn.BatchNorm1d, nn.BatchNorm2d)):
+        m.weight.data.fill_(1)
+        if m.bias is not None:
+            m.bias.data.fill_(0)
+
+
 def transform(image, resize=None):
     from PIL import Image
 
