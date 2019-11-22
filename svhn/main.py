1133f20c998bdfe96c6d86f9e562550fbc92f2ce
augustdemi <augustdemi@gmail.com>
2019-11-22 15:34:53 -0500

    model name lr

--- svhn/main.py
+++ svhn/main.py
@@ -73,9 +73,9 @@ EPS = 1e-9
 CUDA = torch.cuda.is_available()
 
 # path parameters
-MODEL_NAME = 'svhn-run_id%d-priv%02ddim-label_frac%s-sup_frac%s-lamb_text%s-beta1%s-beta2%s-seed%s-bs%s-wseed%s' % (
+MODEL_NAME = 'svhn-run_id%d-priv%02ddim-label_frac%s-sup_frac%s-lamb_text%s-beta1%s-beta2%s-seed%s-bs%s-wseed%s-lr%s' % (
     args.run_id, args.n_private, args.label_frac, args.sup_frac, args.lambda_text, args.beta1, args.beta2, args.seed,
-    args.batch_size, args.wseed)
+    args.batch_size, args.wseed, args.lr)
 
 DATA_PATH = '../data'
 
