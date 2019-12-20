dataset prior, softmax bug

cub_img_attr2/main.py
@@ -84,7 +84,6 @@ MODEL_NAME = 'cub_ia2-run_id%d-privA%02ddim-lamb%s-beta%s-lr%s-bs%s-wseed%s-seed
     args.lr, args.batch_size, args.wseed, args.seed)
 
 if len(args.run_desc) > 1:
-    print(args.run_desc)
     desc_file = os.path.join(args.ckpt_path, 'run_id' + str(args.run_id) + '.txt')
     with open(desc_file, 'w') as outfile:
         outfile.write(args.run_desc)
@@ -361,9 +360,7 @@ def train(data, encA, decA, encB, decB, optimizer):
             poe_cnt = 0
             for i in range(N_ATTR):
                 for j in range(ATTR_DIM[i]):
-                    # prior_logit = torch.log(ATTR_PRIOR[i][j] + EPS)
-                    prior_logit = torch.zeros_like(
-                        q['sharedA_attr0'].dist.logits)  # prior is the concrete dist. of uniform dist.
+                    prior_logit = torch.log(ATTR_PRIOR[i][j] + EPS)
                     poe_logit = q['sharedA_attr' + str(poe_cnt)].dist.logits + q[
                         'sharedB_attr' + str(poe_cnt)].dist.logits + prior_logit
                     q.concrete(logits=poe_logit,
@@ -502,9 +499,9 @@ def save_ckpt(e):
 
 ####
 encA.resnet.load_state_dict(torch.load(
-    '../weights/cub/cub-run_id5-privA100dim-lamb1.0_500.0_5000.0-beta1.0_10.0_1.0-lr0.001-bs50-wseed0-seed0-encA_res_epoch380.rar'))
+    '../weights/cub/cub-run_id5-privA100dim-lamb1.0_500.0_5000.0-beta1.0_10.0_1.0-lr0.001-bs50-wseed0-seed0-encA_res_epoch400.rar'))
 decA.layers.load_state_dict(torch.load(
-    '../weights/cub/cub-run_id5-privA100dim-lamb1.0_500.0_5000.0-beta1.0_10.0_1.0-lr0.001-bs50-wseed0-seed0-decA_layers_epoch380.rar'))
+    '../weights/cub/cub-run_id5-privA100dim-lamb1.0_500.0_5000.0-beta1.0_10.0_1.0-lr0.001-bs50-wseed0-seed0-decA_layers_epoch400.rar'))
 
 if args.ckpt_epochs > 0:
     if CUDA:

cub_img_attr2/model.py
@@ -154,7 +154,7 @@ class DecoderA(nn.Module):
 
             for i in range(len(shared[shared_from])):
                 shared_name = shared[shared_from][i]
-                one_attr_zShared = p.concrete(logits=torch.zeros_like(q[shared_name].dist.logits),
+                one_attr_zShared = p.concrete(logits=torch.log(attr_prior[i] + EPS),
                                               temperature=self.digit_temp,
                                               value=q[shared_name],
                                               name=shared_name)
@@ -263,7 +263,7 @@ class DecoderB(nn.Module):
                 if p[shared_name] is not None:
                     one_attr_zShared = p[shared_name].value
                 else:
-                    one_attr_zShared = p.concrete(logits=torch.zeros_like(q[shared_name].dist.logits),
+                    one_attr_zShared = p.concrete(logits=torch.log(attr_prior[i] + EPS),
                                                   temperature=self.digit_temp,
                                                   value=q[shared_name],
                                                   name=shared_name)
@@ -275,7 +275,6 @@ class DecoderB(nn.Module):
 
             hiddens = self.dec_hidden(torch.cat(latents, -1))
             pred_labels = self.dec_label(hiddens)
-            pred_labels = F.log_softmax(pred_labels + EPS, dim=2)
             pred_labels = pred_labels.squeeze(0)
 
             pred_labels = F.logsigmoid(pred_labels + EPS)