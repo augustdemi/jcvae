5bac5c4466cc206691aa0eed019a0cd037113a4c
augustdemi <augustdemi@gmail.com>
2020-01-29 14:21:32 -0500

    celeba_clsf

---
+++ celeba_classifier/model.py
@@ -0,0 +1,55 @@
+import torch
+import torch.nn as nn
+import sys
+
+sys.path.append('../')
+import probtorch
+from probtorch.util import expand_inputs, normal_init, kaiming_init
+from torch.nn import functional as F
+
+EPS = 1e-9
+TEMP = 0.66
+
+
+class EncoderA(nn.Module):
+    def __init__(self, seed,
+                 n_attr):
+        super(self.__class__, self).__init__()
+        self.digit_temp = torch.tensor(TEMP)
+        self.seed = seed
+
+        self.enc_hidden = nn.Sequential(
+            nn.Conv2d(3, 32, 4, 2, 1, bias=False),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Conv2d(64, 128, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.Conv2d(128, 256, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU())
+
+        self.fc = nn.Sequential(
+            nn.Linear(256 * 5 * 5, 512),
+            nn.ReLU(),
+            nn.Dropout(p=0.1),
+            nn.Linear(512, n_attr))
+
+        self.weight_init()
+
+    def weight_init(self):
+        for m in self._modules:
+            if isinstance(self._modules[m], nn.Sequential):
+                for one_module in self._modules[m]:
+                    kaiming_init(one_module, self.seed)
+            else:
+                kaiming_init(self._modules[m], self.seed)
+
+    def forward(self, x, num_samples=None, q=None):
+        hiddens = self.enc_hidden(x)
+        hiddens = hiddens.view(hiddens.size(0), -1)
+        prediction = self.fc(hiddens)
+        prediction = F.logsigmoid(prediction + EPS)
+        return prediction
