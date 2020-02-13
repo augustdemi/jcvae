fc4b8d58f23a14ca7f0b4f68c917e73417cccda4
augustdemi <augustdemi@gmail.com>
2020-02-13 10:46:37 -0500

    cub image Auto Encoder

---
+++ cub_img_ae/main.py
@@ -0,0 +1,316 @@
+from torchvision import datasets, transforms
+
+import time
+import random
+import torch
+import os
+import numpy as np
+from model import EncoderA, DecoderA
+from datasets import datasets
+import torch.nn as nn
+import sys
+
+sys.path.append('../')
+import probtorch
+import util
+import visdom
+
+# ------------------------------------------------
+# training parameters
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--run_id', type=int, default=2, metavar='N',
+                        help='run_id')
+    parser.add_argument('--run_desc', type=str, default='',
+                        help='run_id desc')
+    parser.add_argument('--n_privateA', type=int, default=636,
+                        help='size of the latent embedding of privateA')
+    parser.add_argument('--batch_size', type=int, default=50, metavar='N',
+                        help='input batch size for training [default: 100]')
+    parser.add_argument('--ckpt_epochs', type=int, default=0, metavar='N',
+                        help='number of epochs to train [default: 200]')
+    parser.add_argument('--epochs', type=int, default=1, metavar='N',
+                        help='number of epochs to train [default: 200]')
+    parser.add_argument('--lr', type=float, default=1e-3, metavar='LR',
+                        help='learning rate [default: 1e-3]')
+
+    parser.add_argument('--beta', type=str, default='1,10,1',
+                        help='beta for TC. [img, attr, label]')
+    parser.add_argument('--lamb', type=str, default='1,500,5000',
+                        help='lambda for reconst. [img, attr, label')
+
+    parser.add_argument('--seed', type=int, default=0, metavar='N',
+                        help='random seed for get_paired_data')
+    parser.add_argument('--wseed', type=int, default=0, metavar='N',
+                        help='random seed for weight')
+
+    parser.add_argument('--ckpt_path', type=str, default='../weights/cub',
+                        help='save and load path for ckpt')
+    parser.add_argument('--gpu', type=str, default='',
+                        help='cuda')
+    parser.add_argument('--outgpu', type=int, default=-1,
+                        help='outgpu')
+    parser.add_argument('--data_path', type=str, default='../../data/cub/CUB_200_2011/CUB_200_2011/',
+                        help='data path')
+    # visdom
+    parser.add_argument('--viz_on',
+                        default=False, type=probtorch.util.str2bool, help='enable visdom visualization')
+    parser.add_argument('--viz_port',
+                        default=8002, type=int, help='visdom port number')
+    args = parser.parse_args()
+
+# ------------------------------------------------
+
+
+EPS = 1e-9
+
+if len(args.gpu) > 0:
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
+    GPU = [int(elt) for elt in args.gpu.split(',')]
+    print('GPU:', GPU)
+else:
+    GPU = []
+CUDA = torch.cuda.is_available()
+
+beta = [float(i) for i in args.beta.split(',')]
+lamb = [float(i) for i in args.lamb.split(',')]
+
+# path parameters
+MODEL_NAME = 'cub-img-ae-resnet50'
+if len(args.run_desc) > 1:
+    desc_file = os.path.join(args.ckpt_path, 'run_id' + str(args.run_id) + '.txt')
+    with open(desc_file, 'w') as outfile:
+        outfile.write(args.run_desc)
+
+N_CLASSES = 200
+N_PIXELS = 3 * 128 * 128
+TEMP = 0.66
+NUM_SAMPLES = 1
+import pickle
+
+path = args.data_path
+ATTR_PRIOR = pickle.load(open(path + "attributes/attr_prior.pkl", "rb"))
+for i in range(len(ATTR_PRIOR)):
+    ATTR_PRIOR[i] = torch.FloatTensor(ATTR_PRIOR[i])
+    if CUDA:
+        ATTR_PRIOR[i] = ATTR_PRIOR[i].cuda()
+
+primary_attr = ['eye_color', 'bill_length', 'shape', 'breast_pattern', 'belly_pattern', 'bill_shape',
+                'bill_color', 'throat_color', 'crown_color', 'forehead_color', 'underparts_color', 'primary_color',
+                'breast_color', 'wing_color']
+# original
+primary_attr = ['eye_color', 'bill_length', 'size', 'shape', 'breast_pattern', 'belly_pattern', 'bill_shape',
+                'bill_color', 'throat_color', 'crown_color', 'forehead_color', 'underparts_color', 'primary_color',
+                'breast_color', 'wing_color']
+# regressor + stat 10
+primary_attr = ['eye_color', 'bill_length', 'leg_color', 'bill_shape',
+                'bill_color', 'throat_color', 'crown_color', 'forehead_color',
+                'belly_color', 'wing_color']
+
+# regressor + stat 13
+primary_attr = ['eye_color', 'bill_length', 'shape', 'bill_shape', 'head_pattern',
+                'bill_color', 'throat_color', 'crown_color', 'forehead_color', 'breast_color',
+                'belly_color', 'wing_color', 'leg_color']
+
+ATTR_IDX = []
+ATTR_DIM = []
+N_ATTR = len(primary_attr)
+TRAIN_CLASSES = np.genfromtxt(path + 'attributes/trainvalids.txt', delimiter='\n', dtype=int)
+
+attributes = np.genfromtxt(path + 'attributes/attr.txt', delimiter='\n', dtype=str)
+total_attr = []
+for i in range(attributes.shape[0]):
+    attr = attributes[i].split("::")[0]
+    total_attr.append(attr)
+    if attr in primary_attr:
+        ATTR_IDX.append(i)
+        ATTR_DIM.append(len(attributes[i].split("::")[1].split(',')) + 1)
+
+ATTR_PRIOR = [ATTR_PRIOR[i] for i in ATTR_IDX]
+print(primary_attr)
+print(ATTR_IDX)
+print(np.array(total_attr)[ATTR_IDX])
+print(len(ATTR_IDX))
+
+
+# visdom setup
+def viz_init():
+    VIZ.close(env=MODEL_NAME + '/lines', win=WIN_ID['total_losses'])
+
+
+def visualize_line():
+    data = LINE_GATHER.data
+
+    epoch = torch.Tensor(data['epoch'])
+    test_total_loss = torch.Tensor(data['test_total_loss'])
+    total_loss = torch.Tensor(data['total_loss'])
+
+    total_losses = torch.tensor(np.stack([total_loss, test_total_loss], -1))
+
+    VIZ.line(
+        X=epoch, Y=total_losses, env=MODEL_NAME + '/lines',
+        win=WIN_ID['total_losses'], update='append',
+        opts=dict(xlabel='epoch', ylabel='loss',
+                  title='Total Loss', legend=['train_loss', 'test_loss'])
+    )
+
+
+if args.viz_on:
+    WIN_ID = dict(
+        total_losses='win_total_losses'
+    )
+    LINE_GATHER = probtorch.util.DataGather(
+        'epoch',
+        'total_loss', 'test_total_loss'
+    )
+    VIZ = visdom.Visdom(port=args.viz_port)
+    viz_init()
+
+train_data = torch.utils.data.DataLoader(datasets(path, ATTR_IDX, train=True, crop=1.2), batch_size=args.batch_size,
+                                         shuffle=True,
+                                         num_workers=len(GPU))
+test_data = torch.utils.data.DataLoader(datasets(path, ATTR_IDX, train=False, crop=1.2), batch_size=args.batch_size,
+                                        shuffle=True,
+                                        num_workers=len(GPU))
+
+BIAS_TRAIN = (train_data.dataset.__len__() - 1) / (args.batch_size - 1)
+BIAS_TEST = (test_data.dataset.__len__() - 1) / (args.batch_size - 1)
+
+
+def cuda_tensors(obj):
+    for attr in dir(obj):
+        value = getattr(obj, attr)
+        if isinstance(value, torch.Tensor):
+            setattr(obj, attr, value.cuda())
+
+
+encA = EncoderA(args.wseed)
+decA = DecoderA(args.wseed)
+
+if CUDA:
+    encA.cuda()
+    decA.cuda()
+    cuda_tensors(encA)
+    cuda_tensors(decA)
+    if len(args.gpu) > 2:
+        print('multi: ' + args.gpu)
+        encA = nn.DataParallel(encA)
+        decA = nn.DataParallel(decA)
+
+optimizer = torch.optim.Adam(
+    list(
+        encA.parameters()) + list(decA.parameters()),
+    lr=args.lr)
+
+
+def recon_loss(recon, orig):
+    return (torch.log(recon + EPS) * orig +
+            torch.log(1 - recon + EPS) * (1 - orig)).sum(-1)
+
+
+def train(data, encA, decA, optimizer):
+    epoch_loss = 0.0
+    encA.train()
+    decA.train()
+
+    N = 0
+
+    torch.autograd.set_detect_anomaly(True)
+    for b, (images, _, _) in enumerate(data):
+        if images.size()[0] == args.batch_size:
+            N += 1
+            optimizer.zero_grad()
+            if CUDA:
+                images = images.cuda()
+            feature = encA(images)
+            recon_images = decA(feature)
+            recon_images = recon_images.view(recon_images.size(0), -1)
+            images = images.view(images.size(0), -1)
+            loss = recon_loss(recon_images, images).mean()
+            loss.backward()
+            optimizer.step()
+            if CUDA:
+                loss = loss.cpu()
+            epoch_loss += loss.item()
+    return epoch_loss / N
+
+
+def test(data, encA, decA):
+    encA.eval()
+    decA.eval()
+    epoch_loss = 0.0
+    N = 0
+    for b, (images, _, _) in enumerate(data):
+        if images.size()[0] == args.batch_size:
+            N += 1
+            if CUDA:
+                images = images.cuda()
+            feature = encA(images)
+            recon_images = decA(feature)
+            recon_images = recon_images.view(recon_images.size(0), -1)
+            images = images.view(images.size(0), -1)
+            loss = recon_loss(recon_images, images)
+            if CUDA:
+                loss = loss.cpu()
+            epoch_loss += loss.item()
+    return epoch_loss / N
+
+
+####
+def save_ckpt(e):
+    torch.save(encA, '%s/%s-encA_epoch%s.rar' % (args.ckpt_path, MODEL_NAME, e))
+    torch.save(decA, '%s/%s-decA_epoch%s.rar' % (args.ckpt_path, MODEL_NAME, e))
+
+
+####
+if args.ckpt_epochs > 0:
+    if CUDA:
+        encA = torch.load('%s/%s-encA_epoch%s.rar' % (args.ckpt_path, MODEL_NAME, args.ckpt_epochs))
+        decA = torch.load('%s/%s-decA_epoch%s.rar' % (args.ckpt_path, MODEL_NAME, args.ckpt_epochs))
+    else:
+        encA = torch.load('%s/%s-encA_epoch%s.rar' % (args.ckpt_path, MODEL_NAME, args.ckpt_epochs), map_location='cpu')
+        decA = torch.load('%s/%s-decA_epoch%s.rar' % (args.ckpt_path, MODEL_NAME, args.ckpt_epochs), map_location='cpu')
+
+for e in range(args.ckpt_epochs, args.epochs):
+
+    train_start = time.time()
+    train_loss = train(train_data, encA, decA, optimizer)
+    train_end = time.time()
+    test_start = time.time()
+    test_loss = test(test_data, encA, decA)
+    test_end = time.time()
+
+    if args.viz_on:
+        LINE_GATHER.insert(epoch=e,
+                           test_total_loss=test_loss,
+                           total_loss=train_loss,
+                           )
+        visualize_line()
+        LINE_GATHER.flush()
+
+    if (e + 1) % 10 == 0 or e + 1 == args.epochs:
+        save_ckpt(e + 1)
+        util.evaluation.save_recon_cub_ae(e, test_data, encA, decA, CUDA, MODEL_NAME,
+                                          fixed_idxs=[658, 1570, 2233, 2456, 2880, 1344, 2750, 1800, 1111, 300, 700,
+                                                      1270, 2133, 2856, 2680, 1300])
+        util.evaluation.save_recon_cub_ae(e, train_data, encA, decA, CUDA, MODEL_NAME,
+                                          fixed_idxs=[130, 215, 502, 537, 4288, 1000, 2400, 1220, 3002, 3312, 230, 415,
+                                                      602, 737, 3288, 1500])
+
+    print('[Epoch %d] Train loss: %.4e (%ds) Test loss: %.4e (%ds)' % (
+        e, train_loss, train_end - train_start,
+        test_loss, test_end - test_start))
+
+if args.ckpt_epochs == args.epochs:
+    util.evaluation.save_recon_cub_ae(args.epochs, test_data, encA, decA, CUDA, MODEL_NAME,
+                                      fixed_idxs=[658, 1570, 2233, 2456, 2880, 1344, 2750, 1800, 1111, 300, 700, 1270,
+                                                  2133, 2856, 2680, 1300])
+    util.evaluation.save_recon_cub_ae(args.epochs, train_data, encA, decA, CUDA, MODEL_NAME,
+                                      fixed_idxs=[130, 215, 502, 537, 4288, 1000, 2400, 1220, 3002, 3312, 230, 415, 602,
+                                                  737, 3288, 1500])
+
+else:
+    save_ckpt(args.epochs)
---
+++ cub_img_ae/model.py
@@ -0,0 +1,242 @@
+import numpy as np
+import torch
+import torch.nn as nn
+
+import sys
+
+sys.path.append('../')
+import probtorch
+from probtorch.util import expand_inputs, normal_init, kaiming_init
+from torch.nn import functional as F
+from torchvision import models
+
+EPS = 1e-9
+
+
+class EncoderA(nn.Module):
+    def __init__(self, seed):
+        super(self.__class__, self).__init__()
+        self.seed = seed
+        resnet = models.resnet50(pretrained=True)
+        modules = list(resnet.children())[:-1]  # we do not use the last fc layer.
+        self.resnet = nn.Sequential(*modules)
+        self.weight_init()
+
+    def weight_init(self):
+        for m in self._modules:
+            if m == 'resnet':
+                continue
+            if isinstance(self._modules[m], nn.Sequential):
+                for one_module in self._modules[m]:
+                    kaiming_init(one_module, self.seed)
+            else:
+                kaiming_init(self._modules[m], self.seed)
+
+    # @expand_inputs
+    def forward(self, x, num_samples=None, q=None):
+        feature = self.resnet(x)
+        feature = feature.view(feature.size(0), -1)
+        return feature
+
+
+class DecoderA(nn.Module):
+    def __init__(self, seed):
+        super(self.__class__, self).__init__()
+        self.seed = seed
+
+        self.layers = nn.ModuleList()
+
+        n_modules = [3, 4, 6, 4]  # num of modules in ResNet50 for seq 1,2,3,4
+        n_channels = [[256, 64, 64, 256], [512, 128, 128, 512], [1024, 256, 256, 1024], [2048, 512, 512, 2048]]
+        kernel_size = [1, 3, 1]
+
+        # avgpool
+        self.layers.append(nn.Sequential(nn.Upsample(scale_factor=4)))
+
+        # en_seq2~ en_seq4
+        for i in range(3, 0, -1):
+            n_module = n_modules[i]
+            n_channel = n_channels[i]
+            n = 0
+            while n < n_module:
+                if n == n_module - 1:
+                    n_channel[-1] = n_channel[-1] // 2
+                    self.layers.append(nn.Sequential(ResBlk(kernel_size, n_channel, upsample=True)))
+                else:
+                    self.layers.append(nn.Sequential(ResBlk(kernel_size, n_channel)))
+                n += 1
+
+        # seq1
+        n_module = n_modules[0]
+        n_channel = n_channels[0]
+        n = 0
+        while n < n_module - 1:
+            self.layers.append(nn.Sequential(ResBlk(kernel_size, n_channel)))
+            n += 1
+
+        # last module of seq 1
+        self.layers.append(nn.Sequential(ResBlk(kernel_size, [256, 64, 64, 64])))
+
+        # maxpool
+        self.layers.append(nn.Sequential(nn.Upsample(scale_factor=2)))
+
+        # first conv
+        conv_list = [nn.ConvTranspose2d(64, 3, 4, stride=2, padding=1),
+                     nn.BatchNorm2d(3), nn.Sigmoid()]
+        self.layers.append(nn.Sequential(*conv_list))
+
+        self.weight_init()
+
+    def weight_init(self):
+        for m in self._modules:
+            if isinstance(self._modules[m], nn.Sequential):
+                for one_module in self._modules[m]:
+                    kaiming_init(one_module, self.seed)
+            else:
+                kaiming_init(self._modules[m], self.seed)
+
+    def forward(self, features):
+        x = features.view(-1, 2048, 1, 1)
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
+
+''' introVAE: https://github.com/woxuankai/IntroVAE-Pytorch/blob/758328a64dbe4eb650a0916af8ce64f5df9d07e3/model.py '''
+
+
+class ResBlk(nn.Module):
+    def __init__(self, kernels, chs, upsample=False):
+        """
+        :param kernels: [1, 3, 3], as [kernel_1, kernel_2, kernel_3]
+        :param chs: [ch_in, 64, 64, 64], as [ch_in, ch_out1, ch_out2, ch_out3]
+        :return:
+        """
+        assert len(chs) - 1 == len(kernels), "mismatching between chs and kernels"
+        assert all(map(lambda x: x % 2 == 1, kernels)), "odd kernel size only"
+        super(ResBlk, self).__init__()
+
+        layers = []
+        for idx in range(len(kernels)):
+            if upsample and idx == 1:
+                layers += [nn.ConvTranspose2d(chs[1], chs[2], 4, stride=2, padding=1),
+                           nn.BatchNorm2d(chs[2]), nn.ReLU(inplace=True)]
+                # layers += [nn.Conv2d(chs[idx], chs[idx + 1], kernels[idx], padding=kernels[idx] // 2, bias=False),
+                #            nn.BatchNorm2d(chs[idx + 1]), nn.ReLU(inplace=True)]
+                # layers += [nn.Upsample(scale_factor=2, mode='nearest')]
+            else:
+                layers += [nn.Conv2d(chs[idx], chs[idx + 1], kernels[idx], padding=kernels[idx] // 2),
+                           nn.BatchNorm2d(chs[idx + 1]), nn.ReLU(inplace=True)]
+        layers.pop()  # remove last activation
+        self.net = nn.Sequential(*layers)
+
+        self.shortcut = nn.Sequential()
+        if chs[0] != chs[-1]:  # convert from ch_int to ch_out3
+            if upsample:
+                self.shortcut.add_module('shortcut_conv', nn.ConvTranspose2d(chs[0], chs[-1], 4, stride=2, padding=1))
+            else:
+                self.shortcut.add_module('shortcut_conv', nn.Conv2d(chs[0], chs[-1], kernel_size=1, padding=0))
+            self.shortcut.add_module('shortcut_bn', nn.BatchNorm2d(chs[-1]))
+            # if upsample:
+            # self.shortcut.add_module('shortcut_up', nn.Upsample(scale_factor=2, mode='nearest'))
+            # self.outAct = nn.LeakyReLU(0.2, True)
+
+    def forward(self, x):
+        # aa =self.shortcut(x)
+        # bb =self.net(x)
+        return F.relu(self.shortcut(x) + self.net(x), inplace=True)  # Identity + residual
+
+
+class DecoderA2(nn.Module):
+    def __init__(self, seed, zPrivate_dim, zSharedAttr_dim, zSharedLabel_dim):
+        super(self.__class__, self).__init__()
+        self.digit_temp = TEMP
+        self.zSharedAttr_dim = zSharedAttr_dim
+        self.zSharedLabel_dim = zSharedLabel_dim
+
+        self.style_mean = zPrivate_dim
+        self.style_std = zPrivate_dim
+        self.seed = seed
+
+        self.fc = nn.Sequential(
+            nn.Linear(zPrivate_dim + sum(zSharedAttr_dim) + zSharedLabel_dim, 2048),
+            nn.ReLU()
+        )
+
+        self.dec_image = nn.Sequential(
+            nn.ConvTranspose2d(512, 256, 4, 2, 1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(256, 128, 4, 2, 1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(128, 64, 4, 2, 1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(64, 64, 4, 2, 1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(64, 32, 4, 2, 1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(32, 3, 4, 2, 1),
+            nn.Sigmoid())
+
+        self.weight_init()
+
+    def weight_init(self):
+        for m in self._modules:
+            if isinstance(self._modules[m], nn.Sequential):
+                for one_module in self._modules[m]:
+                    kaiming_init(one_module, self.seed)
+            else:
+                kaiming_init(self._modules[m], self.seed)
+
+    def forward(self, images, shared, attr_prior, q=None, p=None, num_samples=None):
+        priv_mean = torch.zeros_like(q['privateA'].dist.loc)
+        priv_std = torch.ones_like(q['privateA'].dist.scale)
+
+        p = probtorch.Trace()
+
+        # prior for z_private
+        zPrivate = p.normal(priv_mean,
+                            priv_std,
+                            value=q['privateA'],
+                            name='privateA')
+
+        for shared_from in shared.keys():
+            latents = [zPrivate]
+            # prior for z_shared_atrr
+
+            for i in range(len(shared[shared_from][0])):
+                shared_name = shared[shared_from][0][i]
+                one_attr_zShared = p.concrete(logits=torch.log(attr_prior[i] + EPS),
+                                              temperature=self.digit_temp,
+                                              value=q[shared_name],
+                                              name=shared_name)
+
+                if 'poe' in shared_from:
+                    latents.append(torch.pow(one_attr_zShared + EPS, 1 / 3))  # (sample)^(1/3):prior, modalA,B
+                else:
+                    latents.append(one_attr_zShared)
+            label_zShared = p.concrete(logits=torch.zeros_like(q[shared[shared_from][1]].value),
+                                       temperature=self.digit_temp,
+                                       value=q[shared[shared_from][1]],
+                                       name=shared[shared_from][1])
+            if 'poe' in shared_from:
+                latents.append(torch.pow(label_zShared + EPS, 1 / 4))  # (sample)^(1/4):prior, modalA,B,C
+            else:
+                latents.append(label_zShared)
+
+            hiddens = self.fc(torch.cat(latents, -1))
+            hiddens = hiddens.view(hiddens.size(1), 512, 2, 2)
+            x = self.dec_image(hiddens)
+
+            images_mean = x.view(x.size(0), -1)
+            images = images.view(images.size(0), -1)
+            # define reconstruction loss (log prob of bernoulli dist)
+            p.loss(lambda x_hat, x: -(torch.log(x_hat + EPS) * x +
+                                      torch.log(1 - x_hat + EPS) * (1 - x)).sum(-1),
+                   images_mean, images, name='images_' + shared_from)
+        return p
+
+    def forward2(self, latents, cuda):
+        hiddens = self.fc(torch.cat(latents, -1))
+        hiddens = hiddens.view(hiddens.size(1), 512, 2, 2)
+        x = self.dec_image(hiddens)
+        return x
