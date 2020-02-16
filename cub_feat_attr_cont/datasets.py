057c3bca4ec7c2fdf7ba897e2f17dd715e9a4eff
augustdemi <augustdemi@gmail.com>
2020-02-15 21:44:40 -0500

    cont

---
+++ cub_feat_attr_cont/datasets.py
@@ -0,0 +1,109 @@
+"""
+This script generates a dataset similar to the MultiMNIST dataset
+described in [1]. However, we remove any translation.
+
+[1] Eslami, SM Ali, et al. "Attend, infer, repeat: Fast scene
+understanding with generative models." Advances in Neural Information
+Processing Systems. 2016.
+"""
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+import pickle
+import numpy as np
+
+import torch
+import h5py
+from scipy import io, misc
+from torchvision import transforms
+from torch.utils.data.dataset import Dataset
+# from utils import transform
+from PIL import Image
+
+import random
+
+
+class datasets(Dataset):
+    def __init__(self, path, primary_attr_idx, train=True, crop=None, val=False):
+        self.filepaths, self.attributes, self.labels, self.boxes = load_data(train, path, primary_attr_idx)
+        self.path = path
+        self.crop = crop
+
+        if train:
+            n_data = self.filepaths.shape[0]
+            n_tr_data = int(n_data * 0.8)
+
+            total_idx = list(range(n_data))
+            random.seed(324)
+            random.shuffle(total_idx)
+
+            tr_idx = total_idx[:n_tr_data]
+            val_idx = total_idx[n_tr_data:]
+            if val:
+                self.filepaths = self.filepaths[val_idx]
+                self.attributes = self.attributes[val_idx]
+                self.labels = self.labels[val_idx]
+                self.boxes = self.boxes[val_idx]
+            else:
+                self.filepaths = self.filepaths[tr_idx]
+                self.attributes = self.attributes[tr_idx]
+                self.labels = self.labels[tr_idx]
+                self.boxes = self.boxes[tr_idx]
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+        Returns:
+            tuple: (image, target) where target is index of the target class.
+        """
+        # index = 2233
+        # img_idx: 8055
+        # paht: 138.Tree_Swallow / Tree_Swallow_0060_134961.jpg
+        # label:138
+        # bding box: 13.0 39.0 476.0 458.0
+        # attr: checked
+        img_feat, attr, label, box = self.filepaths[index], self.attributes[index], self.labels[index], self.boxes[
+            index]
+
+        label = torch.tensor(label - 1, dtype=torch.int64)
+        # attr = torch.FloatTensor(attr)
+        return img_feat, attr, label
+
+    def __len__(self):
+        return len(self.labels)
+
+
+def load_data(train, path, primary_attr_idx):
+    boxes = []
+    for line in open(path + 'bounding_boxes.txt', 'r'):
+        box = []
+        for val in line.split(' ')[1:]:
+            box.append(float(val))
+        boxes.append(box)
+    boxes = np.array(boxes)
+    imgid_label = np.array([int(elt.split(' ')[1]) for elt in
+                            np.genfromtxt(path + "image_class_labels.txt", delimiter='\n', dtype=str)])
+
+    if train:
+        vec_attr = pickle.load(open(path + "feature/vec_attr_trainval.pkl", "rb"))
+        hf = h5py.File(path + 'feature/trainval_feat.h5', 'r')
+    else:
+        vec_attr = pickle.load(open(path + "feature/vec_attr_test.pkl", "rb"))
+        hf = h5py.File(path + 'feature/test_feat.h5', 'r')
+
+    attributes = []
+    for key in vec_attr.keys():
+        bin_attr = []
+        for i in primary_attr_idx:
+            bin_attr.extend(vec_attr[key][i][:-1])
+        attributes.append(bin_attr)
+
+    img_feat = np.array(hf['feature'])
+    index = np.array(hf['index'])
+    labels = list(imgid_label[index])
+    boxes = list(boxes[index])
+
+    return img_feat, np.array(attributes), np.array(labels), np.array(boxes)
---
+++ cub_feat_attr_cont/model.py
@@ -0,0 +1,223 @@
+import numpy as np
+import torch
+import torch.nn as nn
+
+import sys
+
+sys.path.append('../')
+import probtorch
+from probtorch.util import expand_inputs, normal_init, kaiming_init
+from torch.nn import functional as F
+from torchvision import models
+
+EPS = 1e-9
+TEMP = 0.66
+
+
+class EncoderA(nn.Module):
+    def __init__(self, seed, zPrivate_dim, zShared_dim, num_hidden):
+        super(self.__class__, self).__init__()
+        self.digit_temp = torch.tensor(TEMP)
+        self.zPrivate_dim = zPrivate_dim
+        self.zShared_dim = zShared_dim
+        self.seed = seed
+
+        self.fc = nn.Sequential(
+            nn.Linear(2048, num_hidden),  # 1560: as CADA_VAE use 1560 hidden
+            nn.ReLU(),
+            nn.Linear(num_hidden, 2 * zPrivate_dim + 2 * zShared_dim),
+            nn.ReLU()
+        )
+
+        self.weight_init()
+
+    def weight_init(self):
+        for m in self._modules:
+            if m == 'resnet':
+                continue
+            if isinstance(self._modules[m], nn.Sequential):
+                for one_module in self._modules[m]:
+                    kaiming_init(one_module, self.seed)
+            else:
+                kaiming_init(self._modules[m], self.seed)
+
+    # @expand_inputs
+    def forward(self, x, num_samples=None, q=None):
+        if q is None:
+            q = probtorch.Trace()
+        stats = self.fc(x)
+        stats = stats.unsqueeze(0)
+        muPrivate = stats[:, :, :self.zPrivate_dim]
+        logvarPrivate = stats[:, :, self.zPrivate_dim:(2 * self.zPrivate_dim)]
+        stdPrivate = torch.sqrt(torch.exp(logvarPrivate) + EPS)
+
+        muShared = stats[:, :, (2 * self.zPrivate_dim):(2 * self.zPrivate_dim + self.zShared_dim)]
+        logvarShared = stats[:, :, (2 * self.zPrivate_dim + self.zShared_dim):]
+        stdShared = torch.sqrt(torch.exp(logvarShared) + EPS)
+
+        q.normal(loc=muPrivate,
+                 scale=stdPrivate,
+                 name='privateA')
+
+        # attributes
+        q.normal(loc=muShared,
+                 scale=stdShared,
+                 name='sharedA')
+        return q
+
+
+class DecoderA(nn.Module):
+    def __init__(self, seed, zPrivate_dim, zShared_dim, num_hidden):
+        super(self.__class__, self).__init__()
+        self.digit_temp = TEMP
+        self.zShared_dim = zShared_dim
+
+        self.style_mean = zPrivate_dim
+        self.style_std = zPrivate_dim
+        self.seed = seed
+
+        self.dec_hidden = nn.Sequential(
+            nn.Linear(zPrivate_dim + zShared_dim, num_hidden),
+            nn.ReLU()
+        )
+
+        self.dec_image = nn.Sequential(
+            nn.Linear(num_hidden, 2048)
+        )
+
+        self.weight_init()
+
+    def weight_init(self):
+        for m in self._modules:
+            if isinstance(self._modules[m], nn.Sequential):
+                for one_module in self._modules[m]:
+                    kaiming_init(one_module, self.seed)
+            else:
+                kaiming_init(self._modules[m], self.seed)
+
+    def forward(self, images, shared, q=None, p=None, num_samples=None):
+        priv_mean = torch.zeros_like(q['privateA'].dist.loc)
+        priv_std = torch.ones_like(q['privateA'].dist.scale)
+        shared_mean = torch.zeros_like(q['sharedA'].dist.loc)
+        shared_std = torch.ones_like(q['sharedA'].dist.scale)
+        p = probtorch.Trace()
+
+        # prior for z_private
+        zPrivate = p.normal(priv_mean,
+                            priv_std,
+                            value=q['privateA'],
+                            name='privateA')
+
+        for shared_from in shared.keys():
+            latents = [zPrivate]
+            # prior for z_shared_atrr
+            zShared = p.normal(shared_mean,
+                               shared_std,
+                               value=q[shared[shared_from]],
+                               name=shared[shared_from])
+            latents.append(zShared)
+
+            hiddens = self.dec_hidden(torch.cat(latents, -1))
+            pred_imgs = self.dec_image(hiddens)
+            pred_imgs = pred_imgs.squeeze(0)
+            pred_imgs = F.logsigmoid(pred_imgs + EPS)
+
+            p.loss(
+                lambda y_pred, target: F.binary_cross_entropy_with_logits(y_pred, target, reduction='none').sum(dim=1), \
+                pred_imgs, images, name='images_' + shared_from)
+        return p
+
+    def forward2(self, latents, cuda):
+        hiddens = self.fc(torch.cat(latents, -1))
+        x = hiddens.view(-1, 2048, 1, 1)
+        for layer in self.layers:
+            x = layer(x)
+
+        return x
+
+
+class EncoderB(nn.Module):
+    def __init__(self, seed, zShared_dim, num_hidden):
+        super(self.__class__, self).__init__()
+        self.digit_temp = torch.tensor(TEMP)
+        self.zShared_dim = zShared_dim
+        self.seed = seed
+        self.enc_hidden = nn.Sequential(
+            nn.Linear(312, num_hidden),
+            nn.ReLU(),
+        )
+
+        self.fc = nn.Linear(num_hidden, zShared_dim * 2)
+        self.weight_init()
+
+    def weight_init(self):
+        for m in self._modules:
+            if isinstance(self._modules[m], nn.Sequential):
+                for one_module in self._modules[m]:
+                    kaiming_init(one_module, self.seed)
+            else:
+                kaiming_init(self._modules[m], self.seed)
+
+    @expand_inputs
+    def forward(self, attributes, num_samples=None, q=None):
+        if q is None:
+            q = probtorch.Trace()
+        # attributes = attributes.view(attributes.size(0), -1)
+        hiddens = self.enc_hidden(attributes)
+        stats = self.fc(hiddens)
+        muShared = stats[:, :, :self.zShared_dim]
+        logvarShared = stats[:, :, self.zShared_dim:]
+        stdShared = torch.sqrt(torch.exp(logvarShared) + EPS)
+        # attributes
+        q.normal(loc=muShared,
+                 scale=stdShared,
+                 name='sharedB')
+        return q
+
+
+class DecoderB(nn.Module):
+    def __init__(self, seed, zShared_dim, num_hidden):
+        super(self.__class__, self).__init__()
+        self.digit_temp = TEMP
+        self.seed = seed
+        self.zShared_dim = zShared_dim
+
+        self.dec_hidden = nn.Sequential(
+            nn.Linear(zShared_dim, num_hidden),
+            nn.ReLU(),
+        )
+        self.dec_label = nn.Sequential(
+            nn.Linear(num_hidden, 312))
+        self.weight_init()
+
+    def weight_init(self):
+        for m in self._modules:
+            if isinstance(self._modules[m], nn.Sequential):
+                for one_module in self._modules[m]:
+                    kaiming_init(one_module, self.seed)
+            else:
+                kaiming_init(self._modules[m], self.seed)
+
+    def forward(self, attributes, shared, q=None, p=None, num_samples=None, train=True):
+        shared_mean = torch.zeros_like(q['sharedB'].dist.loc)
+        shared_std = torch.ones_like(q['sharedB'].dist.scale)
+        p = probtorch.Trace()
+
+        for shared_from in shared.keys():
+            # prior for z_shared_atrr
+            zShared = p.normal(shared_mean,
+                               shared_std,
+                               value=q[shared[shared_from]],
+                               name=shared[shared_from])
+            hiddens = self.dec_hidden(zShared)
+            pred_labels = self.dec_label(hiddens)
+            pred_labels = pred_labels.squeeze(0)
+
+            pred_labels = F.logsigmoid(pred_labels + EPS)
+
+            p.loss(
+                lambda y_pred, target: F.binary_cross_entropy_with_logits(y_pred, target, reduction='none').sum(dim=1), \
+                pred_labels, attributes, name='attr_' + shared_from)
+            pred_labels = torch.round(torch.exp(pred_labels))
+            acc = (pred_labels == attributes).sum() / 312
+        return p, acc
