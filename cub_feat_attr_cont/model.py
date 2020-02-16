45950fada25ec87465ce2fdeb3e85042a65d6931
augustdemi <augustdemi@gmail.com>
2020-02-15 22:23:17 -0500

    bug

--- cub_feat_attr_cont/model.py
+++ cub_feat_attr_cont/model.py
@@ -203,7 +203,6 @@ class DecoderB(nn.Module):
         shared_mean = torch.zeros_like(q['sharedB'].dist.loc)
         shared_std = torch.ones_like(q['sharedB'].dist.scale)
         p = probtorch.Trace()
-        CUDA = torch.cuda.is_available()
 
         for shared_from in shared.keys():
             # prior for z_shared_atrr
@@ -223,9 +222,5 @@ class DecoderB(nn.Module):
             pred_labels = torch.round(torch.exp(pred_labels))
 
             acc = (pred_labels == attributes).sum() / 312
-
-            if CUDA:
-                attributes = attributes.cpu()
-                pred_labels = pred_labels.cpu()
-            f1 = f1_score(attributes.detach().numpy(), pred_labels.detach().numpy(), average="samples")
+            f1 = f1_score(attributes.cpu().detach().numpy(), pred_labels.cpu().detach().numpy(), average="samples")
         return p, acc, f1
