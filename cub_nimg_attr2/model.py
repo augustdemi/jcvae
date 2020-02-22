3dbf43b9be55ad3d7e91b03f5467f36f7baf7f06
augustdemi <augustdemi@gmail.com>
2020-02-22 17:22:57 -0500

    dec는 simple batchnorm, attr에  hidden추가

--- cub_nimg_attr2/model.py
+++ cub_nimg_attr2/model.py
@@ -71,149 +71,61 @@ class EncoderA(nn.Module):
         return q
 
 
-class DecoderA(nn.Module):
-    def __init__(self, seed, zPrivate_dim, zShared_dim, num_hidden):
-        super(self.__class__, self).__init__()
-        self.digit_temp = TEMP
-        self.zShared_dim = zShared_dim
-
-        self.style_mean = zPrivate_dim
-        self.style_std = zPrivate_dim
-        self.seed = seed
-
-        self.fc = nn.Sequential(
-            nn.Linear(zPrivate_dim + zShared_dim, 2048),
-            nn.ReLU()
-        )
-
-        self.layers = nn.ModuleList()
-
-        n_modules = [3, 4, 6, 4]  # num of modules in ResNet50 for seq 1,2,3,4
-        n_channels = [[256, 64, 64, 256], [512, 128, 128, 512], [1024, 256, 256, 1024], [2048, 512, 512, 2048]]
-        kernel_size = [1, 3, 1]
-
-        # avgpool
-        self.layers.append(nn.Sequential(nn.Upsample(scale_factor=4)))
-
-        # en_seq2~ en_seq4
-        for i in range(3, 0, -1):
-            n_module = n_modules[i]
-            n_channel = n_channels[i]
-            n = 0
-            while n < n_module:
-                if n == n_module - 1:
-                    n_channel[-1] = n_channel[-1] // 2
-                    self.layers.append(nn.Sequential(ResBlk(kernel_size, n_channel, upsample=True)))
-                else:
-                    self.layers.append(nn.Sequential(ResBlk(kernel_size, n_channel)))
-                n += 1
-
-        # seq1
-        n_module = n_modules[0]
-        n_channel = n_channels[0]
-        n = 0
-        while n < n_module - 1:
-            self.layers.append(nn.Sequential(ResBlk(kernel_size, n_channel)))
-            n += 1
-
-        # last module of seq 1
-        self.layers.append(nn.Sequential(ResBlk(kernel_size, [256, 64, 64, 64])))
-
-        # maxpool
-        self.layers.append(nn.Sequential(nn.Upsample(scale_factor=2)))
-
-        # first conv
-        conv_list = [nn.ConvTranspose2d(64, 3, 4, stride=2, padding=1),
-                     nn.BatchNorm2d(3), nn.Sigmoid()]
-        self.layers.append(nn.Sequential(*conv_list))
-
-        self.weight_init()
-
-    def weight_init(self):
-        for m in self._modules:
-            if isinstance(self._modules[m], nn.Sequential):
-                for one_module in self._modules[m]:
-                    kaiming_init(one_module, self.seed)
-            else:
-                kaiming_init(self._modules[m], self.seed)
-
-    def forward(self, images, shared, q=None, p=None, num_samples=None):
-        priv_mean = torch.zeros_like(q['privateA'].dist.loc)
-        priv_std = torch.ones_like(q['privateA'].dist.scale)
-        shared_mean = torch.zeros_like(q['sharedA'].dist.loc)
-        shared_std = torch.ones_like(q['sharedA'].dist.scale)
-
-        p = probtorch.Trace()
-
-        # prior for z_private
-        zPrivate = p.normal(priv_mean,
-                            priv_std,
-                            value=q['privateA'],
-                            name='privateA')
-
-        for shared_from in shared.keys():
-            latents = [zPrivate]
-            # prior for z_shared_atrr
-            zShared = p.normal(shared_mean,
-                               shared_std,
-                               value=q[shared[shared_from]],
-                               name=shared[shared_from])
-            latents.append(zShared)
-
-            hiddens = self.fc(torch.cat(latents, -1))
-            x = hiddens.view(-1, 2048, 1, 1)
-            for layer in self.layers:
-                x = layer(x)
-
-            images_mean = x.view(x.size(0), -1)
-            images = images.view(images.size(0), -1)
-            # define reconstruction loss (log prob of bernoulli dist)
-            p.loss(lambda x_hat, x: -(torch.log(x_hat + EPS) * x +
-                                      torch.log(1 - x_hat + EPS) * (1 - x)).sum(-1),
-                   images_mean, images, name='images_' + shared_from)
-        return p
-
-    def forward2(self, latents):
-        hiddens = self.fc(torch.cat(latents, -1))
-        x = hiddens.view(-1, 2048, 1, 1)
-        for layer in self.layers:
-            x = layer(x)
-        return x
-
-
-#
 # class DecoderA(nn.Module):
 #     def __init__(self, seed, zPrivate_dim, zShared_dim, num_hidden):
 #         super(self.__class__, self).__init__()
+#         self.digit_temp = TEMP
 #         self.zShared_dim = zShared_dim
 #
 #         self.style_mean = zPrivate_dim
 #         self.style_std = zPrivate_dim
 #         self.seed = seed
 #
-#         self.dec_hidden = nn.Sequential(
+#         self.fc = nn.Sequential(
 #             nn.Linear(zPrivate_dim + zShared_dim, 2048),
 #             nn.ReLU()
 #         )
 #
-#         self.dec_image = nn.Sequential(
-#             nn.ConvTranspose2d(512, 256, 4, 2, 1, bias=False),
-#             nn.BatchNorm2d(256),
-#             nn.ReLU(),
-#             nn.ConvTranspose2d(256, 128, 4, 2, 1, bias=False),
-#             nn.BatchNorm2d(128),
-#             nn.ReLU(),
-#             nn.ConvTranspose2d(128, 64, 4, 2, 1, bias=False),
-#             nn.BatchNorm2d(64),
-#             nn.ReLU(),
-#             nn.ConvTranspose2d(64, 64, 4, 2, 1, bias=False),
-#             nn.BatchNorm2d(64),
-#             nn.ReLU(),
-#             nn.ConvTranspose2d(64, 32, 4, 2, 1, bias=False),
-#             nn.BatchNorm2d(32),
-#             nn.ReLU(),
-#             nn.ConvTranspose2d(32, 3, 4, 2, 1, bias=False),
-#             nn.Sigmoid())
+#         self.layers = nn.ModuleList()
+#
+#         n_modules = [3, 4, 6, 4]  # num of modules in ResNet50 for seq 1,2,3,4
+#         n_channels = [[256, 64, 64, 256], [512, 128, 128, 512], [1024, 256, 256, 1024], [2048, 512, 512, 2048]]
+#         kernel_size = [1, 3, 1]
+#
+#         # avgpool
+#         self.layers.append(nn.Sequential(nn.Upsample(scale_factor=4)))
+#
+#         # en_seq2~ en_seq4
+#         for i in range(3, 0, -1):
+#             n_module = n_modules[i]
+#             n_channel = n_channels[i]
+#             n = 0
+#             while n < n_module:
+#                 if n == n_module - 1:
+#                     n_channel[-1] = n_channel[-1] // 2
+#                     self.layers.append(nn.Sequential(ResBlk(kernel_size, n_channel, upsample=True)))
+#                 else:
+#                     self.layers.append(nn.Sequential(ResBlk(kernel_size, n_channel)))
+#                 n += 1
+#
+#         # seq1
+#         n_module = n_modules[0]
+#         n_channel = n_channels[0]
+#         n = 0
+#         while n < n_module - 1:
+#             self.layers.append(nn.Sequential(ResBlk(kernel_size, n_channel)))
+#             n += 1
+#
+#         # last module of seq 1
+#         self.layers.append(nn.Sequential(ResBlk(kernel_size, [256, 64, 64, 64])))
+#
+#         # maxpool
+#         self.layers.append(nn.Sequential(nn.Upsample(scale_factor=2)))
+#
+#         # first conv
+#         conv_list = [nn.ConvTranspose2d(64, 3, 4, stride=2, padding=1),
+#                      nn.BatchNorm2d(3), nn.Sigmoid()]
+#         self.layers.append(nn.Sequential(*conv_list))
 #
 #         self.weight_init()
 #
@@ -230,6 +142,7 @@ class DecoderA(nn.Module):
 #         priv_std = torch.ones_like(q['privateA'].dist.scale)
 #         shared_mean = torch.zeros_like(q['sharedA'].dist.loc)
 #         shared_std = torch.ones_like(q['sharedA'].dist.scale)
+#
 #         p = probtorch.Trace()
 #
 #         # prior for z_private
@@ -247,23 +160,110 @@ class DecoderA(nn.Module):
 #                                name=shared[shared_from])
 #             latents.append(zShared)
 #
-#             hiddens = self.dec_hidden(torch.cat(latents, -1))
-#             x = hiddens.view(-1, 512, 2, 2)
-#             images_mean = self.dec_image(x)
+#             hiddens = self.fc(torch.cat(latents, -1))
+#             x = hiddens.view(-1, 2048, 1, 1)
+#             for layer in self.layers:
+#                 x = layer(x)
 #
-#             images_mean = images_mean.view(images_mean.size(0), -1)
+#             images_mean = x.view(x.size(0), -1)
 #             images = images.view(images.size(0), -1)
 #             # define reconstruction loss (log prob of bernoulli dist)
-#             p.loss(lambda x_hat, x: - ((torch.log(x_hat + EPS) * x +
-#                                         torch.log(1 - x_hat + EPS) * (1 - x)).sum(-1)) / N_PIXELS,
+#             p.loss(lambda x_hat, x: -(torch.log(x_hat + EPS) * x +
+#                                       torch.log(1 - x_hat + EPS) * (1 - x)).sum(-1),
 #                    images_mean, images, name='images_' + shared_from)
 #         return p
 #
 #     def forward2(self, latents):
-#         hiddens = self.dec_hidden(torch.cat(latents, -1))
-#         x = hiddens.view(-1, 512, 2, 2)
-#         images_mean = self.dec_image(x)
-#         return images_mean
+#         hiddens = self.fc(torch.cat(latents, -1))
+#         x = hiddens.view(-1, 2048, 1, 1)
+#         for layer in self.layers:
+#             x = layer(x)
+#         return x
+
+
+
+class DecoderA(nn.Module):
+    def __init__(self, seed, zPrivate_dim, zShared_dim, num_hidden):
+        super(self.__class__, self).__init__()
+        self.zShared_dim = zShared_dim
+
+        self.style_mean = zPrivate_dim
+        self.style_std = zPrivate_dim
+        self.seed = seed
+
+        self.dec_hidden = nn.Sequential(
+            nn.Linear(zPrivate_dim + zShared_dim, 2048),
+            nn.ReLU()
+        )
+
+        self.dec_image = nn.Sequential(
+            nn.ConvTranspose2d(512, 256, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.ConvTranspose2d(256, 128, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.ConvTranspose2d(128, 64, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.ConvTranspose2d(64, 64, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.ConvTranspose2d(64, 32, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.ConvTranspose2d(32, 3, 4, 2, 1, bias=False),
+            nn.Sigmoid())
+
+        self.weight_init()
+
+    def weight_init(self):
+        for m in self._modules:
+            if isinstance(self._modules[m], nn.Sequential):
+                for one_module in self._modules[m]:
+                    kaiming_init(one_module, self.seed)
+            else:
+                kaiming_init(self._modules[m], self.seed)
+
+    def forward(self, images, shared, q=None, p=None, num_samples=None):
+        priv_mean = torch.zeros_like(q['privateA'].dist.loc)
+        priv_std = torch.ones_like(q['privateA'].dist.scale)
+        shared_mean = torch.zeros_like(q['sharedA'].dist.loc)
+        shared_std = torch.ones_like(q['sharedA'].dist.scale)
+        p = probtorch.Trace()
+
+        # prior for z_private
+        zPrivate = p.normal(priv_mean,
+                            priv_std,
+                            value=q['privateA'],
+                            name='privateA')
+
+        for shared_from in shared.keys():
+            latents = [zPrivate]
+            # prior for z_shared_atrr
+            zShared = p.normal(shared_mean,
+                               shared_std,
+                               value=q[shared[shared_from]],
+                               name=shared[shared_from])
+            latents.append(zShared)
+
+            hiddens = self.dec_hidden(torch.cat(latents, -1))
+            x = hiddens.view(-1, 512, 2, 2)
+            images_mean = self.dec_image(x)
+
+            images_mean = images_mean.view(images_mean.size(0), -1)
+            images = images.view(images.size(0), -1)
+            # define reconstruction loss (log prob of bernoulli dist)
+            p.loss(lambda x_hat, x: - ((torch.log(x_hat + EPS) * x +
+                                        torch.log(1 - x_hat + EPS) * (1 - x)).sum(-1)) / N_PIXELS,
+                   images_mean, images, name='images_' + shared_from)
+        return p
+
+    def forward2(self, latents):
+        hiddens = self.dec_hidden(torch.cat(latents, -1))
+        x = hiddens.view(-1, 512, 2, 2)
+        images_mean = self.dec_image(x)
+        return images_mean
 
 
 
@@ -272,10 +272,10 @@ class EncoderB(nn.Module):
         super(self.__class__, self).__init__()
         self.zShared_dim = zShared_dim
         self.seed = seed
-        # self.enc_hidden = nn.Sequential(
-        #     nn.Linear(312, num_hidden),
-        #     nn.ReLU(),
-        # )
+        self.enc_hidden = nn.Sequential(
+            nn.Linear(312, num_hidden),
+            nn.ReLU(),
+        )
 
         self.fc = nn.Linear(312, zShared_dim * 2)
         self.weight_init()
@@ -293,8 +293,8 @@ class EncoderB(nn.Module):
         if q is None:
             q = probtorch.Trace()
         # attributes = attributes.view(attributes.size(0), -1)
-        # hiddens = self.enc_hidden(attributes)
-        stats = self.fc(attributes)
+        hiddens = self.enc_hidden(attributes)
+        stats = self.fc(hiddens)
 
         muShared = stats[:, :, :self.zShared_dim]
         logvarShared = stats[:, :, self.zShared_dim:]
@@ -312,10 +312,10 @@ class DecoderB(nn.Module):
         self.seed = seed
         self.zShared_dim = zShared_dim
 
-        # self.dec_hidden = nn.Sequential(
-        #     nn.Linear(zShared_dim, num_hidden),
-        #     nn.ReLU(),
-        # )
+        self.dec_hidden = nn.Sequential(
+            nn.Linear(zShared_dim, num_hidden),
+            nn.ReLU(),
+        )
         self.dec_label = nn.Sequential(
             nn.Linear(zShared_dim, 312))
         self.weight_init()
@@ -339,8 +339,8 @@ class DecoderB(nn.Module):
                                value=q[shared[shared_from]],
                                name=shared[shared_from])
 
-            # hiddens = self.dec_hidden(zShared)
-            pred_labels = self.dec_label(zShared)
+            hiddens = self.dec_hidden(zShared)
+            pred_labels = self.dec_label(hiddens)
             pred_labels = pred_labels.squeeze(0)
 
             pred_labels = F.logsigmoid(pred_labels + EPS)
