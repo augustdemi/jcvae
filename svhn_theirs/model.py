b756d66452e51bb7e47f922910d5a34c0f065f20
augustdemi <augustdemi@gmail.com>
2020-03-13 03:08:08 -0400

    revert one less layer model

--- svhn_theirs/model.py
+++ svhn_theirs/model.py
@@ -51,19 +51,11 @@ class Encoder(nn.Module):
             nn.Conv2d(32, 64, 4, 2, 1, bias=False),
             nn.ReLU(),
             nn.Conv2d(64, 128, 4, 2, 1, bias=False),
-            nn.ReLU(),
-            nn.Conv2d(128, 256, 4, 2, 1, bias=False),
             nn.ReLU()
         )
-
-        self.fc = nn.Sequential(
-            nn.Linear(256 * 2 * 2, 512),
-            nn.ReLU())
-
-        num_hidden = 512
+        num_hidden = 128 * 4 * 4
         self.digit_log_weights = nn.Linear(num_hidden, num_digits)
         self.digit_temp = torch.tensor(0.66)
-
         self.style_mean = nn.Linear(num_hidden + num_digits, num_style)
         self.style_log_std = nn.Linear(num_hidden + num_digits, num_style)
 
@@ -73,7 +65,6 @@ class Encoder(nn.Module):
         images = images.squeeze(0)
         hiddens = self.enc_hidden(images)
         hiddens = hiddens.view(hiddens.size(0), -1)
-        hiddens = self.fc(hiddens)
         digits = q.concrete(logits=self.digit_log_weights(hiddens).unsqueeze(0),
                             temperature=self.digit_temp,
                             value=labels,
@@ -97,18 +88,18 @@ class Decoder(nn.Module):
         self.digit_temp = 0.66
         self.style_mean = torch.zeros(num_style)
         self.style_std = torch.ones(num_style)
+        num_hidden = 128 * 4 * 4
         self.dec_hidden = nn.Sequential(
-            nn.Linear(num_style + num_digits, 256 * 2 * 2),
+            nn.Linear(num_style + num_digits, num_hidden),
             nn.ReLU())
         self.dec_image = nn.Sequential(
-            nn.ConvTranspose2d(256, 128, 4, 2, 1, bias=False),
-            nn.ReLU(),
             nn.ConvTranspose2d(128, 64, 4, 2, 1, bias=False),
             nn.ReLU(),
             nn.ConvTranspose2d(64, 32, 4, 2, 1, bias=False),
             nn.ReLU(),
             nn.ConvTranspose2d(32, 3, 4, 2, 1, bias=False),
             nn.Sigmoid())
+
     def forward(self, images, q=None, num_samples=None):
         p = probtorch.Trace()
         digits = p.concrete(logits=self.digit_log_weights,
@@ -120,8 +111,8 @@ class Decoder(nn.Module):
                           value=q['styles'],
                           name='styles')
         hiddens = self.dec_hidden(torch.cat([digits, styles], -1))
-        # hiddens = hiddens.view(-1, 128, 4, 4)
-        hiddens = hiddens.view(-1, 256, 2, 2)
+        hiddens = hiddens.view(-1, 128, 4, 4)
+        # hiddens = hiddens.view(-1, 256, 2, 2)
         images_mean = self.dec_image(hiddens)
 
         images_mean = images_mean.view(images_mean.size(0), -1).unsqueeze(0)
