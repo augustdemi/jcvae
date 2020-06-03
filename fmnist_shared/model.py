e40a503dca932ccacaadb043f2862c113a3feb44
augustdemi <augustdemi@gmail.com>
2020-06-03 14:37:54 -0400

    runid 0

--- fmnist_shared/model.py
+++ fmnist_shared/model.py
@@ -200,7 +200,7 @@ class DecoderA(nn.Module):
 
 class EncoderB(nn.Module):
     def __init__(self, seed, num_digis=10,
-                 num_hidden=512,
+                 num_hidden=256,
                  zShared_dim=10):
         super(self.__class__, self).__init__()
         self.digit_temp = torch.tensor(TEMP)
@@ -208,8 +208,6 @@ class EncoderB(nn.Module):
         self.seed = seed
         self.enc_hidden = nn.Sequential(
             nn.Linear(num_digis, num_hidden),
-            nn.ReLU(),
-            nn.Linear(num_hidden, num_hidden),
             nn.ReLU())
 
         self.fc = nn.Linear(num_hidden, 2 * zShared_dim)
@@ -243,7 +241,7 @@ class EncoderB(nn.Module):
 
 class DecoderB(nn.Module):
     def __init__(self, seed, num_digits=10,
-                 num_hidden=512,
+                 num_hidden=256,
                  zShared_dim=10):
         super(self.__class__, self).__init__()
         self.digit_temp = TEMP
@@ -251,13 +249,7 @@ class DecoderB(nn.Module):
 
         self.dec_hidden = nn.Sequential(
             nn.Linear(zShared_dim, num_hidden),
-            nn.ReLU(),
-            nn.Linear(num_hidden, num_hidden),
-            nn.ReLU(),
-            nn.Linear(num_hidden, num_hidden),
-            nn.ReLU()
-
-        )
+            nn.ReLU())
         self.dec_label = nn.Sequential(
             nn.Linear(num_hidden, num_digits))
         self.weight_init()
